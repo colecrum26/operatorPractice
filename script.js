4681d347c66bf8ef8d5a7f7c6c8b916d737e9231
colecrum26 <cwcrumrine@gmail.com>
2023-03-10 12:02:31 -0600

    fixed concatenation

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let lastName2 = "Saget";
 // CREATE A FUNCTION THAT CREATES A NEW ID AND PLACES FIRST NAME AND LAST NAME TOGETHER
 // PA - parameters at the declaration; arguments at the invocation
 function createID(firstName, lastName){
-    console.log("with concatenation" + firstName + " " + lastName);
+    console.log("with concatenation " + firstName + " " + lastName);
     console.log(`with template literals: ${firstName} ${lastName}`)
     console.log(++id);
 }
